c93347176c888780c543d9c4e6e671fed6a419af
Joe <jtillotson@student.neumont.edu>
2018-02-08 09:41:55 -0700

    taco

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,34 @@
 package models;
 
 public class User {
+    private String password;
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString(){
+        return "Username: "+getUserName()+" Password: "+getPassword();
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        User other=(User)obj;
+        return this.getPassword().equals(other.getPassword())&&this.getUserName().equals(other.getUserName());
+    }
 }
