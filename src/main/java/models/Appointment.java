23d412ea603b63568d80b15122a8c6e04c150a61
Setsuna <thedarkness435@gmail.com>
2018-02-12 08:26:23 -0700

    Appointment Complete, Models Should be complete now

--- src/main/java/models/Appointment.java
+++ src/main/java/models/Appointment.java
@@ -1,4 +1,51 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class Appointment implements Business {
+
+    private ArrayList<Provider> providers;
+
+    private Calendar date;
+
+    private ArrayList<Procedure> procedures;
+
+    public ArrayList<Provider> getProviders() {
+        return providers;
+    }
+
+    public void setProviders(ArrayList<Provider> providers) {
+        this.providers = providers;
+    }
+
+    public ArrayList<Procedure> getProcedures() {
+        return procedures;
+    }
+
+    public void setProcedures(ArrayList<Procedure> procedures) {
+        this.procedures = procedures;
+    }
+
+    public Calendar getDate() {
+
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Appointment p = (Appointment)obj;
+        return this.getDate() == p.getDate() && this.getProcedures() == p.getProcedures()
+                && this.getProviders() == p.getProviders();
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment Date: " + this.getDate() + ", " + "Appointment Procedures: "
+                + this.getProcedures() + ", " + "Appointment Providers: " + this.getProviders();
+    }
 }
