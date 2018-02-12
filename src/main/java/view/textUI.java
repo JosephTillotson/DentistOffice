8f99fbf215d2e99b8d6bcc52555b9b9563711050
Joe <jtillotson@student.neumont.edu>
2018-02-12 08:05:31 -0700

    Text UI comments

--- src/main/java/view/textUI.java
+++ src/main/java/view/textUI.java
@@ -9,6 +9,13 @@ import java.util.Set;
 public class textUI {
     private BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
+    /**
+     * will present the menu and then go to propmpt for input
+     * @param menuOptions is the Map <> that will be printed
+     * @return will go to the next method to propmpt for input
+     * @throws IOException To make sure that the for loop runs properly and the buffered reader
+     */
+
     public int printMenu(Map<Integer, String> menuOptions) throws IOException {
         for (Integer key : menuOptions.keySet()) {
             System.out.println(key + " - " + menuOptions.get(key));
@@ -16,6 +23,12 @@ public class textUI {
         return readInt(menuOptions.keySet());
     }
 
+    /**
+     * This prompts the user to get the imput of the desired int.
+     * @param allowedInts it will take in the menu options from above to get the desired ints that are needed and are only allowed
+     * @return will return a int that are in the desired amount
+     * @throws IOException Trows it from the buffered reader
+     */
     private int readInt(Set<Integer> allowedInts) throws IOException {
         while (true) {
             String line = br.readLine();
@@ -33,9 +46,18 @@ public class textUI {
 
     }
 
+    /**
+     * will display to the user the input that we need.
+     * @param s the string to be printed
+     */
     public void displayToUser(String s) {
         System.out.println(s);
     }
+
+    /**
+     * will promt for a int without having to go through the process of teh maps
+     * @return will return one int. will need to be clarified in the main class
+     */
 
     public int getInt() {
         try {
