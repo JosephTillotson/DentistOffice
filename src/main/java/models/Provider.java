6c94350acd3126ee81a20f0aeb687e2ed13cd594
Joe <jtillotson@student.neumont.edu>
2018-02-12 09:12:34 -0700

    User comments and checking

--- src/main/java/models/Provider.java
+++ src/main/java/models/Provider.java
@@ -7,36 +7,80 @@ public class Provider implements Business{
 
     private String title;
 
+    /**
+     * basic constrctor for Provider
+     */
     public Provider(){}
+
+    /**
+     * overloaded constructer for Provider
+     * @param name name of the provider
+     * @param title title of the Provider
+     * @param id id of the Provider.
+     */
     public Provider(String name, String title, int id){
         this.setName(name);
         this.setId(id);
         this.setTitle(title);
     }
 
+    /**
+     * will get the title of the Provider
+     * @return will return the title of the Provider
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * will Set the title of the Provider
+     * @param title the title to be set for the provider
+     */
+
     public void setTitle(String title) {
+        if (title==null){
+            throw new IllegalArgumentException("Title cant ve null");
+        }
         this.title = title;
     }
 
+    /**
+     * will return the name of the Provider
+     * @return return the name of the Provider
+     */
     public String getName() {
 
         return name;
     }
 
+    /**
+     * will set the name of the Provider
+     * @param name the name of lthe provider to be
+     */
     public void setName(String name) {
+        if (name==null){
+            throw new IllegalArgumentException("name cant be null");
+        }
         this.name = name;
     }
 
+    /**
+     * will return teh Id of the Provider
+     * @return the id of the user
+     */
     public int getId() {
 
         return id;
     }
 
+    /**
+     * will set the Id of the user
+     * @param id the ID to be
+     */
     public void setId(int id) {
+        if (id<0){
+            throw new IllegalArgumentException("id cant be less then 0");
+        }
         this.id = id;
     }
 
