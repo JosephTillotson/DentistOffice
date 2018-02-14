5882c0b573da57c239ab331768cc637dfcd18c20
Joe <jtillotson@student.neumont.edu>
2018-02-13 23:16:53 -0700

    SEARCH IS DONE

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -1,7 +1,10 @@
 package test;
 
+import controller.Controller;
+
 public class test {
     public static void main(String[] args) {
-
+        Controller controller= new Controller();
+        controller.run();
     }
 }
