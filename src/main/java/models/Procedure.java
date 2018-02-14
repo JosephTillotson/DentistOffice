ea2bb180f3a071c48ae0045cc227afc58ab5c025
Setsuna <thedarkness435@gmail.com>
2018-02-13 23:13:36 -0700

    Controller in Progress, anything that has to do with Appointment is not finished, Tuned the other classes to get it working with controller and fixed uncaught errors in search class.

--- src/main/java/models/Procedure.java
+++ src/main/java/models/Procedure.java
@@ -4,6 +4,9 @@ public class Procedure implements Business{
     private Patient patient;
     private Provider provider;
     private String id;
+private String name;
+
+
 
     /**
      * this is the basic constructor for Procedure
@@ -14,22 +17,15 @@ public class Procedure implements Business{
      * overloaded constructor for Procedure
      * @param id id of the Procedure
      * @param name of the procedure
-     * @param id2 id of the person
-     * @param phonenumber of the person
-     * @param email of the person
-     * @param name2 of the person
-     * @param groupID the persons Group Id
-     * @param memberID the Persons member ID
-     * @param paymentCard Persons paymentcard
-     * @param amount Person amount owed
-     * @param title title of the provider
-     * @param id3 the id of the provider
-     * @param name3 name of the provider
+     * @param patient info of Patient Object
+     * @param provider info of Provider Object
      */
-    public Procedure(String id,String name, int id2, String phonenumber, String email, String name2,int groupID,int memberID,String paymentCard, int amount,String title, int id3, String name3){
-        setId(id);
-        patient = new Patient(name,email, paymentCard,id2,phonenumber,groupID,name2, memberID, amount);
-        provider=new Provider(name3,title,id3);
+    public Procedure(String id,String name, Patient patient, Provider provider) {
+        this.setId(id);
+        this.setName(name);
+
+
+
     }
 
     /**
@@ -47,6 +43,14 @@ public class Procedure implements Business{
     public void setId(String id) {
 
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
